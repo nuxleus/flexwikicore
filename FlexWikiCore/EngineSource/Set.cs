450675dc5e2e9ace81120a9e0c387c1a75bfdeb8
candera <candera@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2006-10-14 14:26:25 +0000

    Moved folder remotely.
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/Build1705@336 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ FlexWikiCore/EngineSource/Set.cs
@@ -0,0 +1,69 @@
+#region License Statement
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// The use and distribution terms for this software are covered by the 
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.TXT at the root of this distribution.
+// By using this software in any fashion, you are agreeing to be bound by 
+// the terms of this license.
+//
+// You must not remove this notice, or any other, from this software.
+#endregion
+
+using System;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace FlexWiki
+{
+	public class Set : IEnumerable
+	{
+		HybridDictionary hash = new HybridDictionary();
+
+		public void Add(object obj)
+		{
+			if (hash.Contains(obj))
+				return;
+			hash[obj] = null;
+		}
+
+		public void AddRange(ICollection c)
+		{
+			foreach (object obj in c)
+				Add(obj);
+		}
+
+		public bool Contains(object obj)
+		{
+			return hash.Contains(obj);
+		}
+
+		public int Count
+		{
+			get
+			{
+				return hash.Count;
+			}
+		}
+
+		public IEnumerator GetEnumerator()
+		{
+			return hash.Keys.GetEnumerator();
+		}
+
+		public void Remove(object obj)
+		{
+			hash.Remove(obj);
+		}
+
+		public object First
+		{
+			get
+			{
+				foreach (object obj in hash.Keys)
+					return obj;
+				return null;
+			}
+		}
+	}
+}
