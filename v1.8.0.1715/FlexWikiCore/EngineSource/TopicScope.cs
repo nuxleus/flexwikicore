b9152a03addcf8aa4659f2c02f54f456137b7d7d
candera <candera@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2006-10-14 21:36:08 +0000

    Moved tag folder.
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/v1.8@469 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ v1.8.0.1715/FlexWikiCore/EngineSource/TopicScope.cs
@@ -0,0 +1,53 @@
+#region License Statement
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// The use and distribution terms for this software are covered by the 
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.TXT at the root of this distribution.
+// By using this software in any fashion, you are agreeing to be bound by 
+// the terms of this license.
+//
+// You must not remove this notice, or any other, from this software.
+#endregion
+
+using System;
+using System.Collections;
+
+namespace FlexWiki
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class TopicScope : IScope
+	{
+		public TopicScope(IScope ContainingScope, DynamicTopic topic)
+		{
+			_ContainingScope = ContainingScope;
+			_Topic = topic;
+		}
+
+		IScope _ContainingScope;
+		public IScope ContainingScope
+		{
+			get
+			{
+				return _ContainingScope;
+			}
+		}
+
+		DynamicTopic _Topic;
+		public DynamicTopic Topic
+		{
+			get
+			{
+				return _Topic;
+			}
+		}
+
+		public IBELObject ValueOf(string symbol, ArrayList args, ExecutionContext ctx)
+		{
+			return Topic.ValueOf(symbol, args, ctx);
+		}
+		
+	}
+}
