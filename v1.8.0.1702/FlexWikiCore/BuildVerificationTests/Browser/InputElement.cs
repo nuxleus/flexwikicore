41bdc35cfe10cf3222a85b1dff2a3c51e6d0be37
candera <candera@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2006-10-14 21:31:29 +0000

    Moved tag folder.
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/v1.8@456 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ v1.8.0.1702/FlexWikiCore/BuildVerificationTests/Browser/InputElement.cs
@@ -0,0 +1,71 @@
+#region License Statement
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// The use and distribution terms for this software are covered by the 
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.TXT at the root of this distribution.
+// By using this software in any fashion, you are agreeing to be bound by 
+// the terms of this license.
+//
+// You must not remove this notice, or any other, from this software.
+#endregion
+
+using System;
+using SHDocVw;
+using mshtml;
+
+
+namespace FlexWiki.BuildVerificationTests
+{
+	/// <summary>
+	/// Summary description for InputElement.
+	/// </summary>
+	public class InputElement : HTMLElement
+	{
+		private IHTMLInputElement TypedElement
+		{
+			get
+			{
+				return (IHTMLInputElement)Element;
+			}
+		}
+
+		public InputElement(IHTMLInputElement e, DocumentElement d) : base(e as IHTMLElement, d)
+		{
+		}
+
+    public bool Checked
+    {
+      get 
+      {
+        return TypedElement.@checked; 
+      }
+      set
+      {
+        TypedElement.@checked = value; 
+      }
+    }
+
+    public FormElement Form
+    {
+      get { return new FormElement(TypedElement.form, base.Document); }
+    }
+
+		/// <summary>
+		/// Get or set the value of the input field
+		/// </summary>
+		public string Value
+		{
+			get
+			{
+				return TypedElement.value;
+			}
+			set
+			{
+				TypedElement.value = value;
+			}
+		}
+
+
+	}
+}
