ff3a09263f3b62b25d7c9a1dfdaf1fe9e7f30d94
candera <candera@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2006-10-14 14:26:25 +0000

    Moved folder remotely.
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/v1-8-0-1732@336 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ FlexWikiCore/EngineSource/DuplicateTopicException.cs
@@ -0,0 +1,51 @@
+#region License Statement
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// The use and distribution terms for this software are covered by the 
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.TXT at the root of this distribution.
+// By using this software in any fashion, you are agreeing to be bound by 
+// the terms of this license.
+//
+// You must not remove this notice, or any other, from this software.
+#endregion
+
+using System;
+
+namespace FlexWiki
+{
+	/// <summary>
+	/// Summary description for DuplicateTopicException.
+	/// </summary>
+	public class DuplicateTopicException : ApplicationException
+	{
+		public DuplicateTopicException() : base()
+		{
+		}
+		public DuplicateTopicException(string message) : base(message)
+		{
+		}
+
+		TopicName _Topic;
+
+		public TopicName Topic
+		{
+			get
+			{
+				return _Topic;
+			}
+			set
+			{
+				_Topic = value;
+			}
+		}
+
+		public static DuplicateTopicException ForTopic(TopicName tn)
+		{
+			DuplicateTopicException answer = new DuplicateTopicException("Duplicate topic: " + tn.ToString());
+			answer.Topic = tn;
+			return answer;
+		}
+		
+	}
+}
