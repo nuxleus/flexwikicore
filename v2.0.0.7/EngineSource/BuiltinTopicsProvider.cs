feaf2298a4f1de7a24357791d875a82200d1c62f
fwbuild <fwbuild@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2006-10-18 14:01:14 +0000

    Tagged build
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/v2.0@506 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ v2.0.0.7/EngineSource/BuiltinTopicsProvider.cs
@@ -0,0 +1,391 @@
+using System;
+using System.Collections.Generic;
+using System.IO; 
+using System.Text;
+
+using FlexWiki.Collections;
+
+namespace FlexWiki
+{
+    public class BuiltinTopicsProvider : ContentProviderBase
+    {
+        // Constants
+        #region Constants
+        private const string c_builtInAuthor = "FlexWiki"; 
+        private const string c_defaultHomePageContent = @"@flexWiki=http://www.flexwiki.com/default.aspx/FlexWiki/$$$.html
+
+!About Wiki
+If you're new to WikiWiki@flexWiki, you should read the VisitorWelcome@flexWiki or OneMinuteWiki@flexWiki .  The two most important things to know are 
+        1. follow the links to follow the thoughts and  
+        1. YouAreEncouragedToChangeTheWiki@flexWiki
+
+Check out the FlexWikiFaq@flexWiki as a means  to collaborate on questions you may have on FlexWiki@flexWiki
+";
+        private const string c_defaultNormalBordersContent = @"
+:MenuItem:{ tip, command, url |
+  with (Presentations) 
+  {[
+    ""||{T-}"", 
+    Image(federation.LinkMaker.LinkToImage(""images/go.gif""), command, url), 
+    ""||{+}"", 
+    Link(url, command, tip), 
+    ""||"", 
+    Newline
+  ]}
+}
+
+
+RightBorder:{
+aTopic|
+  [
+  request.IsAuthenticated.IfTrue
+  {[
+    ""||{C2+}"",
+    ""Welcome '''"", 
+    request.AuthenticatedUserName,
+    ""'''"",
+    ""||"",
+    Newline,
+    request.CanLogInAndOut.IfTrue
+    {[	
+      ""||"",
+      with (Presentations)
+      {
+        Link(federation.LinkMaker.LinkToLogoff(aTopic.Namespace.Name), ""Log off"", ""Log off."")
+      },
+      ""||"",
+      Newline
+    ]}
+    IfFalse{""""},
+  ]}
+  IfFalse
+  {
+    """"
+  },
+  namespace.Description,
+  Newline, ""----"", Newline, 
+  federation.About,
+  Newline, ""----"", Newline,
+  ""*Recent Topics*"",
+  Newline,
+  request.UniqueVisitorEvents.Snip(15).Collect
+  { each |
+    [
+    Tab, 
+    ""*"",
+    Presentations.Link(federation.LinkMaker.LinkToTopic(each.Fullname), each.Name),
+    Newline
+    ]
+  }
+  ]
+}
+
+
+LeftBorder:{
+aTopic |
+  [
+request.AreDifferencesShown.IfTrue
+  {
+    MenuItem(""Don't highlight differences between this topic and previous version"", ""Hide Changes"", federation.LinkMaker.LinkToTopic(aTopic.Fullname))
+  }
+  IfFalse
+  {
+    MenuItem(""Show differences between this topic and previous version"", ""Show Changes"", federation.LinkMaker.LinkToTopicWithDiffs(aTopic.Fullname))
+  },
+  aTopic.Version.IfNull
+  {
+    aTopic.Namespace.IsReadOnly.IfFalse
+    {
+      MenuItem(""Edit this topic"", ""Edit"", federation.LinkMaker.LinkToEditTopic(aTopic.Fullname))
+    }
+    IfTrue
+    {
+      """"
+    }
+  }
+  Else
+  {
+    """"
+  },
+  MenuItem(""Show printable view of this topic"", ""Print"", federation.LinkMaker.LinkToPrintView(aTopic.Fullname)),
+  MenuItem(""Show recently changed topics"", ""Recent Changes"", federation.LinkMaker.LinkToRecentChanges(aTopic.Namespace.Name)),
+  MenuItem(""Show RRS feeds to keep up-to-date"", ""Subscriptions"", federation.LinkMaker.LinkToSubscriptions(aTopic.Namespace.Name)),
+  MenuItem(""Show disconnected topics"", ""Lost and Found"", federation.LinkMaker.LinkToLostAndFound(aTopic.Namespace.Name)),
+  MenuItem(""Find references to this topic"", ""Find References"", federation.LinkMaker.LinkToSearchFor(null, aTopic.Name)),
+  aTopic.Namespace.IsReadOnly.IfFalse
+  {
+    MenuItem(""Rename this topic"", ""Rename"", federation.LinkMaker.LinkToRename(aTopic.Fullname))
+  }
+  IfTrue
+  {
+    """"
+  },
+  ""----"", Newline,
+  [
+    ""||{T-}"",
+    ""'''Search'''"", 
+    ""||"",
+    Newline, 
+    ""||{+}"",
+    Presentations.FormStart(federation.LinkMaker.LinkToSearchNamespace(aTopic.Namespace.Name), ""get""),
+    Presentations.HiddenField(""namespace"", aTopic.Namespace.Name),
+    Presentations.InputField(""search"", """", 15),
+    Presentations.ImageButton(""goButton"", federation.LinkMaker.LinkToImage(""images/go-dark.gif""), ""Search for this text""), 
+    Presentations.FormEnd(),
+    ""||"",
+    Newline
+  ],
+  Newline, ""----"", Newline,
+  [
+    ""'''History'''"", Newline,
+    aTopic.Changes.Snip(5).Collect
+    { each |
+      [
+        ""||{T-+}"", 
+        Presentations.Link(federation.LinkMaker.LinkToTopic(each.Fullname), [each.Timestamp].ToString), 
+        ""||"", 
+        Newline,
+        ""||{T-+}``"", 
+        each.Author, 
+        ""``||"", 
+        Newline
+      ]
+    },
+    Newline,
+    MenuItem(""List all versions of this topic"", ""List all versions"", federation.LinkMaker.LinkToVersions(aTopic.Fullname)),
+    aTopic.Version.IfNotNull
+    {[
+      Newline,
+      Presentations.FormStart(federation.LinkMaker.LinkToRestore(aTopic.Fullname), ""post""),
+      Presentations.HiddenField(""RestoreTopic"", aTopic.Fullname),
+      Presentations.SubmitButton(""restoreButton"", ""Restore Version""), 
+      Presentations.FormEnd(),
+    ]}
+    Else
+    {
+      """"
+    },
+    Newline
+  ]
+
+  ]
+}
+";
+        #endregion Constants
+        
+        // Fields
+        private QualifiedTopicName _bordersTopicName;
+        private object _bordersTopicNameLock = new object(); 
+        private QualifiedTopicName _homePageTopicName;
+        private object _homePageTopicNameLock = new object(); 
+
+        // Constructors
+
+        public BuiltinTopicsProvider(ContentProviderBase next)
+            : base(next)
+        {
+        }
+
+
+        // Properties
+        private QualifiedTopicName BordersTopicName
+        {
+            get
+            {
+                // Cache the home page topic name - we don't want to retrieve
+                // it every time because that can result in an infinite loop. 
+                if (_bordersTopicName == null)
+                {
+                    lock (_bordersTopicNameLock)
+                    {
+                        if (_bordersTopicName == null)
+                        {
+                            _bordersTopicName = NamespaceManager.BordersTopicName;
+                        }
+                    }
+                }
+
+                return _bordersTopicName;
+            }
+        }
+
+        private QualifiedTopicName HomePageTopicName
+        {
+            get
+            {
+                // Cache the home page topic name - we don't want to retrieve
+                // it every time because that can result in an infinite loop. 
+                if (_homePageTopicName == null)
+                {
+                    lock (_homePageTopicNameLock)
+                    {
+                        if (_homePageTopicName == null)
+                        {
+                            _homePageTopicName = NamespaceManager.HomePageTopicName;
+                        }
+                    }
+                }
+
+                return _homePageTopicName;
+            }
+        }
+
+        // Methods
+
+        public override TopicChangeCollection AllChangesForTopicSince(UnqualifiedTopicName topic, DateTime stamp)
+        {
+            TopicChangeCollection changes = Next.AllChangesForTopicSince(topic, stamp);
+
+            if (IsBuiltInTopic(topic))
+            {
+                // All the built-in topics have a default revision at DateTime.MinValue. If the 
+                // timestamp is later than that, we don't report back the default revision.
+                if (stamp == DateTime.MinValue)
+                {
+                    if (changes == null)
+                    {
+                        changes = new TopicChangeCollection();
+                    }
+
+                    changes.Add(
+                        new TopicChange(
+                            new QualifiedTopicRevision(
+                                topic.LocalName,
+                                NamespaceManager.Namespace,
+                                QualifiedTopicRevision.NewVersionStringForUser(c_builtInAuthor, DateTime.MinValue)),
+                            DateTime.MinValue,
+                            c_builtInAuthor));
+                }
+            }
+
+            return changes; 
+        }
+        public override QualifiedTopicNameCollection AllTopics()
+        {
+            QualifiedTopicNameCollection topics = Next.AllTopics();
+
+            foreach (QualifiedTopicName builtInTopic in GetBuiltInTopics())
+            {
+                if (!topics.Contains(builtInTopic))
+                {
+                    topics.Add(builtInTopic); 
+                }
+            }
+
+            return topics; 
+        }
+        public override bool IsExistingTopicWritable(UnqualifiedTopicName topic)
+        {
+            // If it's not a built-in topic, just pass the question along
+            if (!IsBuiltInTopic(topic))
+            {
+                return Next.IsExistingTopicWritable(topic); 
+            }
+
+            // Otherwise, if it is a built-in topic, does it exist in the next provider? 
+            if (Next.TopicExists(topic))
+            {
+                // If it does, it's only writable if it's writable there
+                return Next.IsExistingTopicWritable(topic); 
+            }
+            
+            // Otherwise, it's writable only if the next provider is writable as a whole.
+            return !Next.IsReadOnly; 
+
+        }
+        public override TextReader TextReaderForTopic(UnqualifiedTopicRevision topicRevision)
+        {
+            // We need to special-case the definition topic because otherwise we 
+            // cause an infinite loop: this method calls NamespaceManager.HomePage, 
+            // which calls TextReaderForTopic on the definition topic to figure out
+            // which topic is the home page. 
+            if (!IsBuiltInTopic(topicRevision))
+            {
+                return Next.TextReaderForTopic(topicRevision); 
+            }
+            else if (topicRevision.Version == null)
+            {
+                TopicChangeCollection changes = Next.AllChangesForTopicSince(
+                    topicRevision.AsUnqualifiedTopicName(), DateTime.MinValue);
+                if (changes == null || changes.Count == 0)
+                {
+                    string defaultContent = DefaultContentFor(topicRevision.LocalName);
+                    return new StringReader(defaultContent);
+                }
+                else
+                {
+                    return Next.TextReaderForTopic(topicRevision);
+                }
+            }
+            else if (topicRevision.Version == TopicRevision.NewVersionStringForUser(c_builtInAuthor, DateTime.MinValue))
+            {
+                string defaultContent = DefaultContentFor(topicRevision.LocalName);
+                return new StringReader(defaultContent);
+            }
+            else
+            {
+                return Next.TextReaderForTopic(topicRevision);
+            }
+        }
+        public override bool TopicExists(UnqualifiedTopicName name)
+        {
+            if (IsBuiltInTopic(name))
+            {
+                return true; 
+            }
+
+            return Next.TopicExists(name); 
+        }
+
+        private string DefaultContentFor(string topic)
+        {
+            QualifiedTopicName topicName = new QualifiedTopicName(topic, Namespace);
+
+            if (topicName.Equals(HomePageTopicName))
+            {
+                return c_defaultHomePageContent; 
+            }
+            else if (topicName.Equals(BordersTopicName))
+            {
+                return c_defaultNormalBordersContent;
+            }
+            else
+            {
+                return null; 
+            }
+            
+        }
+        private QualifiedTopicNameCollection GetBuiltInTopics()
+        {
+            // We need to build this every time, because the name of the HomePage can change dynamically.
+            QualifiedTopicNameCollection builtInTopics = new QualifiedTopicNameCollection();
+
+            builtInTopics.Add(HomePageTopicName);
+            builtInTopics.Add(BordersTopicName);
+
+            return builtInTopics;
+        }
+        private bool IsBuiltInTopic(UnqualifiedTopicRevision revision)
+        {
+            return IsBuiltInTopic(new UnqualifiedTopicName(revision.LocalName));
+        }
+        private bool IsBuiltInTopic(UnqualifiedTopicName topic)
+        {
+            // This is to prevent infinite recursion - we need to query the definition topic
+            // to figure out the HomePage and the Borders topic. 
+            if (topic.LocalName == NamespaceManager.DefinitionTopicLocalName)
+            {
+                return false; 
+            }
+            QualifiedTopicName qualifiedTopicName = new QualifiedTopicName(topic.LocalName, Namespace);
+            return GetBuiltInTopics().Contains(qualifiedTopicName); 
+        }
+        private bool IsDefinitionTopic(UnqualifiedTopicRevision topicRevision)
+        {
+            QualifiedTopicName topicName = new QualifiedTopicName(topicRevision.LocalName, Namespace);
+            return topicName.Equals(NamespaceManager.DefinitionTopicName);
+        }
+
+
+    }
+}
---
+++ v2.0.0.7/EngineSource/FileInfoTopicData.cs
@@ -0,0 +1,119 @@
+#region License Statement
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// The use and distribution terms for this software are covered by the 
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.TXT at the root of this distribution.
+// By using this software in any fashion, you are agreeing to be bound by 
+// the terms of this license.
+//
+// You must not remove this notice, or any other, from this software.
+#endregion
+
+using System;
+using System.Diagnostics;
+using System.Xml.Serialization;
+using System.Collections;
+using System.Collections.Specialized;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using FlexWiki.Formatting;
+
+
+namespace FlexWiki
+{
+    internal class FileInfoTopicData : TopicData
+    {
+        private static Regex s_historicalFileNameRegex = new Regex("[^(]+\\((?<year>[0-9]{4})-(?<month>[0-9]{2})-(?<day>[0-9]{2})-(?<hour>[0-9]{2})-(?<minute>[0-9]{2})-(?<second>[0-9]{2})(\\.(?<fraction>[0-9]+))?(-(?<name>.*))?\\)");
+
+        private string _namespace;
+        private IFileInformation _fileInformation;
+
+        public FileInfoTopicData(IFileInformation info, string ns)
+        {
+            _fileInformation = info;
+            _namespace = ns;
+        }
+
+
+        public override string Author
+        {
+            get
+            {
+                string filename = _fileInformation.Name;
+                // remove the extension
+                filename = filename.Substring(0, filename.Length - _fileInformation.Extension.Length);
+                Match m = s_historicalFileNameRegex.Match(filename);
+                if (!m.Success)
+                {
+                    return null;
+                }
+                if (m.Groups["name"].Captures.Count == 0)
+                {
+                    return null;
+                }
+                return m.Groups["name"].Value;
+
+            }
+        }
+        public string FullName
+        {
+            get
+            {
+                return _fileInformation.FullName;
+            }
+        }
+        public override DateTime LastModificationTime
+        {
+            get
+            {
+                return _fileInformation.LastWriteTime;
+            }
+        }
+        public override string Name
+        {
+            get
+            {
+                return _fileInformation.NameWithoutExtension;
+            }
+        }
+        public override string Namespace
+        {
+            get
+            {
+                return _namespace;
+            }
+        }
+        public override string Version
+        {
+            get
+            {
+                return ExtractVersionFromHistoricalFilename(_fileInformation.NameWithoutExtension);
+            }
+        }
+
+        private static string ExtractTopicFromHistoricalFilename(string filename)
+        {
+            int p = filename.IndexOf("(");
+            if (p == -1)
+            {
+                return filename;
+            }
+            return filename.Substring(0, p);
+        }
+        private static string ExtractVersionFromHistoricalFilename(string filename)
+        {
+            // ab(xyz)
+            // 
+            int p = filename.IndexOf("(");
+            if (p == -1)
+            {
+                return filename;
+            }
+            int close = filename.LastIndexOf(")");
+            return filename.Substring(p + 1, close - p - 1);
+        }
+
+    }
+}
