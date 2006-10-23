106be0c33bff241172504fbc31cd6adfa524e7b5
fwbuild <fwbuild@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2006-10-23 07:21:09 +0000

    Tagged build
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/v2.0@515 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ v2.0.0.11/FlexWiki.UnitTests/MockTopicRevision.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlexWiki.UnitTests
+{
+    class MockTopicRevision
+    {
+        private string _author;
+        private string _contents;
+        private DateTime _created;
+        private bool _isReadOnly; 
+        private DateTime _modified; 
+        private string _version;
+
+        internal MockTopicRevision(string contents, string author, DateTime created)
+        {
+            _contents = contents;
+            _author = author;
+            _created = created;
+            _modified = created; 
+            _version = TopicRevision.NewVersionStringForUser(author, created);
+        }
+
+        internal string Author
+        {
+            get { return _author; }
+        }
+
+        internal string Contents
+        {
+            get { return _contents; }
+            set { _contents = value; }
+        }
+
+        internal DateTime Created
+        {
+            get { return _created; }
+        }
+
+        internal bool IsReadOnly
+        {
+            get { return _isReadOnly; }
+            set { _isReadOnly = value; }
+        }
+
+        internal DateTime Modified
+        {
+            get { return _modified; }
+            set { _modified = value; } 
+        }
+
+        internal string Version
+        {
+            get
+            {
+                return _version;
+
+            }
+        }
+    }
+}
