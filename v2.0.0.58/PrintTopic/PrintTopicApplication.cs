d31fc3b0d8dfd05b6596623fb9154a8fc8684f25
fwbuild <fwbuild@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2007-07-03 16:24:26 +0000

    Tagged build
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/v2.0@629 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ v2.0.0.58/PrintTopic/PrintTopicApplication.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+using FlexWiki; 
+
+namespace PrintTopic
+{
+    class PrintTopicApplication : IWikiApplication
+    {
+        private readonly string _configPath;
+        private FederationConfiguration _federationConfiguration; 
+        private readonly LinkMaker _linkMaker; 
+        private readonly ITimeProvider _timeProvider = new DefaultTimeProvider();
+
+
+        public PrintTopicApplication(string configPath, LinkMaker linkMaker)
+        {
+            _configPath = configPath;
+            _linkMaker = linkMaker; 
+        }
+
+        public FederationConfiguration FederationConfiguration
+        {
+            get 
+            {
+                if (_federationConfiguration == null)
+                {
+                    XmlSerializer ser = new XmlSerializer(typeof(FederationConfiguration));
+                    using (FileStream fileStream = new FileStream(_configPath, FileMode.Open,
+                        FileAccess.Read, FileShare.Read))
+                    {
+
+                        _federationConfiguration = (FederationConfiguration) ser.Deserialize(fileStream);
+                    }
+                }
+
+                return _federationConfiguration; 
+            }
+        }
+        public bool IsTransportSecure
+        {
+            get { return true; }
+        }
+        public LinkMaker LinkMaker
+        {
+            get { return _linkMaker; }
+        }
+        public OutputFormat OutputFormat
+        {
+            get { return OutputFormat.HTML; }
+        }
+        public ITimeProvider TimeProvider
+        {
+            get { return _timeProvider; }
+        }
+
+        public void Log(string source, LogLevel level, string message)
+        {
+            // no-op
+        }
+        public void LogDebug(string source, string message)
+        {
+            // no-op
+        }
+        public void LogError(string source, string message)
+        {
+            // no-op
+        }
+        public void LogInfo(string source, string message)
+        {
+            // no-op
+        }
+        public void LogWarning(string source, string message)
+        {
+            // no-op
+        }
+        public string ResolveRelativePath(string path)
+        {
+            throw new NotImplementedException(); 
+        }
+        public void WriteFederationConfiguration()
+        {
+            throw new NotImplementedException(); 
+        }
+
+
+    }
+}
