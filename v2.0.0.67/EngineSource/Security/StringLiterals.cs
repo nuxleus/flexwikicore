aac57cb1111352303b0827e8c3492d4b730843ae
fwbuild <fwbuild@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2007-07-25 14:39:12 +0000

    Tagged build
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/v2.0@652 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ v2.0.0.67/EngineSource/Security/StringLiterals.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlexWiki.Security
+{
+    internal static class StringLiterals
+    {
+        internal const string All = "all";
+        internal const string Allow = "Allow";
+        internal const string Anonymous = "anonymous";
+        internal const string Authenticated = "authenticated"; internal const string Read = "Read";
+        internal const string Deny = "Deny";
+        internal const string Edit = "Edit";
+        internal const string ManageNamespace = "ManageNamespace";
+        internal const string RolePrefix = "role:";
+        internal const string UserPrefix = "user:";
+    }
+}
