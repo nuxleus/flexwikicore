7456d4906de58c3fad5d16c51434b784cec96c90
fwbuild <fwbuild@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2007-07-31 17:51:15 +0000

    Tagged build
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/v2.0@664 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ v2.0.0.72/EngineSource/ClassicBehaviors.cs
@@ -0,0 +1,370 @@
+#region License Statement
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// The use and distribution terms for this software are covered by the 
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.TXT at the root of this distribution.
+// By using this software in any fashion, you are agreeing to be bound by 
+// the terms of this license.
+//
+// You must not remove this notice, or any other, from this software.
+#endregion
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Xsl;
+
+using FlexWiki.Collections;
+using FlexWiki.Formatting;
+
+namespace FlexWiki
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ExposedClass("ClassicBehaviors", "A collection of behaviors supported before WikiTalk was invented; mostly for backwards-compatibility")]
+    public class ClassicBehaviors : BELObject
+    {
+        public ClassicBehaviors()
+            : base()
+        {
+        }
+
+
+        [ExposedMethod(ExposedMethodFlags.Default, "Answer a formatted error message")]
+        public IBELObject ErrorMessage(string title, string body)
+        {
+            return new ErrorMessage(title, body);
+        }
+
+        [ExposedMethod(ExposedMethodFlags.Default, "Answer a newline character")]
+        public string Newline
+        {
+            get
+            {
+                return Environment.NewLine;
+            }
+        }
+
+        [ExposedMethod(ExposedMethodFlags.Default, "Answer the current date and time")]
+        public DateTime Now
+        {
+            get
+            {
+                return DateTime.Now;
+            }
+        }
+
+        [ExposedMethod(ExposedMethodFlags.Default, "Answer the name of this product (FlexWiki)")]
+        public string ProductName
+        {
+            get
+            {
+                return "FlexWiki";
+            }
+        }
+
+        [ExposedMethod(ExposedMethodFlags.Default, "Answer the exact version number of this software")]
+        public string ProductVersion
+        {
+            get
+            {
+                return FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).ProductVersion;
+            }
+        }
+
+        [ExposedMethod(ExposedMethodFlags.Default, "Answer a tab character")]
+        public string Tab
+        {
+            get
+            {
+                return "\t";
+            }
+        }
+
+
+        [ExposedMethod(ExposedMethodFlags.NeedContext, "Answer a list of all namespaces in the federation (with details)")]
+        public string AllNamespacesWithDetails(ExecutionContext ctx)
+        {
+            ArrayList files = new ArrayList();
+            StringBuilder b = new StringBuilder();
+            ArrayList bases = new ArrayList();
+            Federation fed = ctx.CurrentFederation;
+            foreach (string ns in fed.Namespaces)
+            {
+                NamespaceManager namespaceManager = fed.NamespaceManagerForNamespace(ns);
+                bases.Add(namespaceManager);
+            }
+            // And now add the namespace map itself
+            bases.Sort();
+            foreach (NamespaceManager info in bases)
+            {
+                b.Append(
+                  "||\"" + info.FriendlyTitle + "\":" + info.Namespace + "." + info.HomePage +
+                  "||" + info.Description +
+                  "||" + (info.Contact == null ? "" : info.Contact) +
+                  "||\n");
+            }
+
+            return b.ToString();
+        }
+
+        [ExposedMethod(ExposedMethodFlags.NeedContext, "Present the given image with the supplied additiona information")]
+        public IBELObject Image(ExecutionContext ctx, string URL, string altText, [ExposedParameter(true)] string width, [ExposedParameter(true)]string height)
+        {
+            return new ImagePresentation(altText, URL, null, height, width);
+        }
+
+        // TODO - This method violates the rules: no behavior should ever directly output presentation strings.  To fix this,
+        // we need a BELHyperlinkPresentation object that this behavior can produce and that can do IOutputSequence...
+        [ExposedMethod(ExposedMethodFlags.AllowsVariableArguments, "Link to the given interWiki")]
+        public IBELObject InterWiki(ExecutionContext ctx, string interWikiName, string linkText)
+        {
+            ExternalReferencesMap map = ctx.ExternalWikiMap;
+            Federation fed = ctx.CurrentFederation;
+
+            string result = null;
+            string safeName = Formatter.EscapeHTML(interWikiName);
+            string safeLinkText = Formatter.EscapeHTML(linkText);
+            if (map != null)
+            {
+                foreach (KeyValuePair<string, string> extWiki in map)
+                {
+                    if (extWiki.Key.ToString().ToUpper() == interWikiName.ToUpper())
+                    {
+                        result = "<a class=ExternalLink title=\"External link to " + safeName + "\" target=\"ExternalLinks\" href=\"" + extWiki.Value.ToString().Replace("$$$", safeLinkText) + "\">" + safeLinkText + "</a>";
+                        break;
+                    }
+                }
+            }
+
+            if (result == null)
+            {
+                string interWikisTopic = fed.Configuration.InterWikisTopic;
+                if (interWikisTopic == null)
+                {
+                    interWikisTopic = "_InterWikis";
+                }
+
+                QualifiedTopicRevision topic = new QualifiedTopicRevision(interWikisTopic, fed.DefaultNamespace);
+                if (!fed.TopicExists(topic))
+                {
+                    throw new ArgumentException("Failed to find InterWikisTopic [" + interWikisTopic + "].");
+                }
+
+                TopicPropertyCollection props = fed.GetTopicProperties(topic);
+                if (props != null)
+                {
+                    foreach (TopicProperty entry in props)
+                    {
+                        if (entry.Name.ToUpper() == interWikiName.ToUpper())
+                        {
+                            result = "<a class=ExternalLink title=\"External link to " + safeName + "\" target=\"ExternalLinks\" href=\"" + entry.LastValue.ToString().Replace("$$$", safeLinkText) + "\">" + safeLinkText + "</a>";
+                        }
+                    }
+                }
+
+                if (result == null)
+                {
+                    throw new ArgumentException("Failed to find InterWiki '" + safeName + "'.");
+                }
+            }
+
+            for (int p = 0; p < ctx.TopFrame.ExtraArguments.Count; p++)
+            {
+                string replacementArg = Formatter.EscapeHTML(ctx.TopFrame.ExtraArguments[p] == null ? "null" : ctx.TopFrame.ExtraArguments[p].ToString());
+                result = result.Replace("$" + (p + 1).ToString(), replacementArg);
+            }
+
+            return new StringPresentation(result);
+        }
+
+        [ExposedMethod(ExposedMethodFlags.NeedContext, "Answer the value of the given property")]
+        public string Property(ExecutionContext ctx, string topic, string property)
+        {
+            TopicName abs = null;
+            bool ambig = false;
+            string answer = null;
+            try
+            {
+                NamespaceManager namespaceManager = ctx.CurrentNamespaceManager;
+                if (namespaceManager == null)
+                {
+                    namespaceManager = ctx.CurrentFederation.DefaultNamespaceManager;
+                }
+                TopicRevision rel = new TopicRevision(topic);
+                abs = namespaceManager.UnambiguousTopicNameFor(rel.LocalName);
+            }
+            catch (TopicIsAmbiguousException)
+            {
+                ambig = true;
+            }
+            if (abs != null)
+            {
+                // Got a unique one!
+                answer = ctx.CurrentFederation.GetTopicPropertyValue(abs, property);
+            }
+            else
+            {
+                if (ambig)
+                {
+                    throw new ArgumentException("Ambiguous topic name: " + topic);
+                }
+                else
+                {
+                    throw new ArgumentException("Unknown topic name: " + topic);
+                }
+            }
+            return answer;
+        }
+
+        [ExposedMethod(ExposedMethodFlags.NeedContext, "Answer a list of topics that match the given criteria.")]
+        public string TopicIndex(ExecutionContext ctx, string type, [ExposedParameter(true)] string arg2, [ExposedParameter(true)] string topicNamespace)
+        {
+            InvocationFrame frame = ctx.TopFrame;
+            bool isTitleType;
+            switch (type)
+            {
+                case "Title":
+                    isTitleType = true;
+                    break;
+
+                case "Property":
+                    isTitleType = false;
+                    break;
+
+                default:
+                    throw (new ArgumentException("Type must be either 'Title' or 'Property' for TopicIndex"));
+            }
+
+            if (!isTitleType && !frame.WasParameterSupplied(2))
+            {
+                throw (new ArgumentException("For 'Property' type of TopicIndex, property name must be supplied as second parameter"));
+            }
+
+            string result = "";
+            ArrayList uniqueNamespaces = new ArrayList();
+            if (topicNamespace == null)
+            {
+                foreach (string ns in ctx.CurrentFederation.Namespaces)
+                {
+                    uniqueNamespaces.Add(ns);
+                }
+                uniqueNamespaces.Sort();
+            }
+            else
+            {
+                uniqueNamespaces.Add(topicNamespace);
+            }
+
+            Regex titleFilter = null;
+            if (isTitleType && arg2 != null)
+            {
+                titleFilter = new Regex(arg2, RegexOptions.Singleline | RegexOptions.Compiled);
+            }
+
+            foreach (string ns in uniqueNamespaces)
+            {
+                NamespaceManager namespaceManager = ctx.CurrentFederation.NamespaceManagerForNamespace(ns);
+                if (namespaceManager == null)
+                {
+                    continue;
+                }
+
+                foreach (TopicName topic in namespaceManager.AllTopics(ImportPolicy.DoNotIncludeImports))
+                {
+                    if (topic.LocalName.StartsWith("_") || (ctx.CurrentTopicName != null && topic.DottedName == ctx.CurrentTopicName.DottedName)) // no sense listing the page we're currently viewing
+                    {
+                        continue;
+                    }
+                    if (isTitleType)
+                    {
+                        if (titleFilter == null || titleFilter.IsMatch(topic.LocalName))
+                        {
+                            result += "\t* \"" + topic.DottedName + "\":" + topic.Namespace + ".[" + topic.LocalName + "]" + Environment.NewLine;
+                        }
+                        string topicSummary = ctx.CurrentFederation.GetTopicPropertyValue(topic, "Summary");
+                        if (!string.IsNullOrEmpty(topicSummary))
+                        {
+                            result += "\t\t*" + topicSummary + Environment.NewLine;
+                        }
+                    }
+                    else
+                    {
+                        string topicProperty = ctx.CurrentFederation.GetTopicPropertyValue(topic, arg2);
+                        if (!string.IsNullOrEmpty(topicProperty))
+                        {
+                            result += "\t* \"" + topic.DottedName + "\":" + topic.Namespace + ".[" + topic.LocalName + "]" + Environment.NewLine + "\t\t* " + topicProperty + Environment.NewLine;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        public override IOutputSequence ToOutputSequence()
+        {
+            return new WikiSequence(ToString());
+        }
+        public override string ToString()
+        {
+            return "classic behaviors object";
+        }
+
+        [ExposedMethod(ExposedMethodFlags.Default, "Answer the result of transforming the given XML using the given transform")]
+        public string XmlTransform(string xmlURL, string xslURL)
+        {
+            string result = null;
+            XmlDocument xmlDoc = new XmlDocument();
+            XslCompiledTransform xslTransform = new XslCompiledTransform();
+
+            try
+            {
+                // Load the XML file
+                xmlDoc.Load(xmlURL);
+            }
+            catch (Exception ex)
+            {
+                result = "Failed to load XML parameter (" + xmlURL + "): " + ex.Message;
+            }
+            if (result == null)
+            {
+                try
+                {
+                    // Load the XSL File
+                    xslTransform.Load(xslURL);
+                }
+                catch (Exception ex)
+                {
+                    result = "Failed to load XSL parameter (" + xslURL + "): " + ex.Message;
+                }
+            }
+            if (result == null)
+            {
+                try
+                {
+                    StringWriter sW = new StringWriter();
+                    XmlTextWriter XmlW = new XmlTextWriter(sW);
+                    xslTransform.Transform(new XmlNodeReader(xmlDoc), new XsltArgumentList(), XmlW, null);
+                    result = sW.ToString();
+                    XmlW.Close();
+                }
+                catch (Exception ex)
+                {
+                    result = "Failed to Transform " + ex.Message;
+                }
+            }
+            return result;
+        }
+
+    }
+}
+
+
+
---
+++ v2.0.0.72/EngineSource/TopicName.cs
@@ -0,0 +1,201 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using FlexWiki.Collections;
+
+namespace FlexWiki
+{
+    /// <summary>
+    /// Represents a topic for which the namespace may or may not be known. 
+    /// </summary>
+    public class TopicName
+    {
+        // Constants
+
+        private const string c_separator = ".";
+
+        // Fields
+
+        private string _localName;
+        private string _namespace;
+
+        // Constructors
+
+        public TopicName()
+        {
+        }
+        public TopicName(string name)
+        {
+            if (name == null)
+            {
+                throw new ArgumentException("A null topic name is not legal."); 
+            }
+
+            int dot = name.LastIndexOf(c_separator);
+            _namespace = null;
+            if (dot >= 0)
+            {
+                _namespace = name.Substring(0, dot);
+                if (_namespace == "")
+                {
+                    _namespace = null;
+                }
+                name = name.Substring(dot + 1);
+            }
+            _localName = name;
+
+        }
+        public TopicName(string localName, string ns)
+        {
+            if (localName.Contains(c_separator))
+            {
+                throw new ArgumentException("An illegal local name was specified: the namespace separator is not allowed as part of a local name."); 
+            }
+
+            _localName = localName;
+            _namespace = ns;
+        }
+
+        // Properties
+
+        public string FormattedName
+        {
+            get
+            {
+                // basic breaking rule: break between lowercase to uppercase transitions
+                // caveat: don't break before first cap
+                bool firstCap = true;
+                bool lastWasLower = false;
+                string formattedName = "";
+                string scan = LocalName;
+                for (int i = 0; i < scan.Length; i++)
+                {
+                    char ch = scan[i];
+                    if (!Char.IsUpper(ch))
+                    {
+                        formattedName += ch;
+                        lastWasLower = true;
+                        continue;
+                    }
+
+                    if (firstCap)
+                    {
+                        firstCap = false;
+                        formattedName += ch;
+                        lastWasLower = false;
+                        continue;
+                    }
+
+                    if (lastWasLower || ((i + 1) < scan.Length && Char.IsLower(scan[i + 1])))
+                    {
+                        formattedName += ' ';
+                    }
+                    formattedName += ch;
+                    lastWasLower = false;
+                }
+                return formattedName;
+
+            }
+        }
+        public bool IsQualified
+        {
+            get { return Namespace != null; }
+        }
+        public string LocalName
+        {
+            get { return _localName; }
+            set { _localName = value; }
+        }
+        public virtual string Namespace
+        {
+            get { return _namespace; }
+            set { _namespace = value; }
+        }
+        public string DottedName
+        {
+            get
+            {
+                if (IsQualified)
+                {
+                    return Namespace + c_separator + LocalName;
+                }
+                else
+                {
+                    return LocalName;
+                }
+            }
+        }
+        protected static string Separator
+        {
+            get { return c_separator; }
+        }
+
+        /// <summary>
+        /// Answer a collection of topic names for the alternate forms of the topic name (e.g., signular forms of plural words)
+        /// </summary>
+        public TopicNameCollection AlternateForms()
+        {
+            TopicNameCollection answer = new TopicNameCollection();
+            if (LocalName.EndsWith("s"))
+            {
+                answer.Add(new TopicName(LocalName.Substring(0, LocalName.Length - 1), Namespace));
+            }
+            if (LocalName.EndsWith("ies"))
+            {
+                answer.Add(new TopicName(LocalName.Substring(0, LocalName.Length - 3) + "y", Namespace));
+            }
+            if (LocalName.EndsWith("sses"))
+            {
+                answer.Add(new TopicName(LocalName.Substring(0, LocalName.Length - 2), Namespace));
+            }
+            if (LocalName.EndsWith("xes"))
+            {
+                answer.Add(new TopicName(LocalName.Substring(0, LocalName.Length - 2), Namespace));
+            }
+            return answer;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            TopicName that = obj as TopicName; 
+
+            if (that == null)
+            {
+                return false; 
+            }
+
+            return this.DottedName == that.DottedName; 
+        }
+        public override int GetHashCode()
+        {
+            string qualifiedName = DottedName;
+
+            if (qualifiedName == null)
+            {
+                return "<<null>>".GetHashCode(); 
+            }
+
+            return qualifiedName.GetHashCode(); 
+        }
+        public QualifiedTopicName ResolveRelativeTo(string ns)
+        {
+            if (IsQualified)
+            {
+                return new QualifiedTopicName(LocalName, Namespace);
+            }
+            else
+            {
+                return new QualifiedTopicName(LocalName, ns);
+            }
+        }
+        public override string ToString()
+        {
+            return DottedName;
+        }
+    }
+}
