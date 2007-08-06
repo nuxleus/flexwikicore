49e4e7ef4dbe7dbff0412e323d46b507689f6710
fwbuild <fwbuild@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2007-08-06 13:27:43 +0000

    Tagged build
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/v1.8@667 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ v1.8.0.1746/SqlProvider.UnitTests/SqlFormatterTests.cs
@@ -0,0 +1,39 @@
+#region License Statement
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// The use and distribution terms for this software are covered by the 
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.TXT at the root of this distribution.
+// By using this software in any fashion, you are agreeing to be bound by 
+// the terms of this license.
+//
+// You must not remove this notice, or any other, from this software.
+#endregion
+using System;
+using System.Collections;
+using FlexWiki;
+using FlexWiki.Formatting;
+using NUnit.Framework;
+using FlexWiki.UnitTests;
+
+namespace FlexWiki.SqlProvider.UnitTests
+{
+	/// <summary>
+	/// Summary description for SqlFormatterTests.
+	/// </summary>
+	[TestFixture]public class SqlFormattingTests: FormattingTests
+	{
+		public SqlFormattingTests()
+		{
+			base.storeType = "sql";
+		}
+	}
+
+	[TestFixture]public class SqlFormattingServicesTests: FormattingServicesTests
+	{
+		public SqlFormattingServicesTests()
+		{
+			base.storeType = "sql";	
+		}
+	}
+}
