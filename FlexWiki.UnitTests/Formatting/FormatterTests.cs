237b2e61bb4b4213de44adec36ec2f39ba3d0a55
jwdavidson <jwdavidson@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2007-08-06 15:28:51 +0000

    Feature 1400618 - DOCTYPE, valid HTML, CSS - at least no quirks mode  (final fixup FormatterTests.cs)
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/trunk@673 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

--- FlexWiki.UnitTests/Formatting/FormatterTests.cs
+++ FlexWiki.UnitTests/Formatting/FormatterTests.cs
@@ -504,7 +504,7 @@ file://servername/umuff&#126;/folder%20name/file.txt",
             FormatTest(
               @"@baf=http://www.baf.com/$$$
 Again, TestIt@baf should be an external link along with TestItAgain@baf, however @this should be code formatted@.",
-              @"<p>Again, <a class=""ExternalLink"" title=""External link to baf"" target=""ExternalLinks"" href=""http://www.baf.com/TestIt"">TestIt</a> should be an external link along with <a class=ExternalLink title=""External link to baf"" target=""ExternalLinks"" href=""http://www.baf.com/TestItAgain"">TestItAgain</a>, however <code>this should be code formatted</code>.</p>
+              @"<p>Again, <a class=""ExternalLink"" title=""External link to baf"" target=""ExternalLinks"" href=""http://www.baf.com/TestIt"">TestIt</a> should be an external link along with <a class=""ExternalLink"" title=""External link to baf"" target=""ExternalLinks"" href=""http://www.baf.com/TestItAgain"">TestItAgain</a>, however <code>this should be code formatted</code>.</p>
 ");
             FormatTest(
               @"@google=http://www.google.com/search?hl=en&ie=UTF-8&oe=UTF-8&q=$$$
