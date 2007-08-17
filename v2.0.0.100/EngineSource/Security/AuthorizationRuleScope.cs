26c672bc986058f9f7a6b90fcf444f63e2812049
fwbuild <fwbuild@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2007-08-17 10:35:13 +0000

    Tagged build
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/v2.0@741 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ v2.0.0.100/EngineSource/Security/AuthorizationRuleScope.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlexWiki.Security
+{
+    public enum AuthorizationRuleScope
+    {
+        Wiki = 0,
+        Namespace = 1,
+        Topic = 2,
+    }
+}
---
+++ v2.0.0.100/FlexWiki.UnitTests/Security/TransportSecurityRequirement.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlexWiki.UnitTests.Security
+{
+    internal enum TransportSecurityRequirement
+    {
+        Unspecified,
+        RequiredOnContent,
+        RequiredOnNone,
+    }
+}
