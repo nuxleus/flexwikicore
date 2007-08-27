1fa1a861f443ac9b73aa1554ea30afad0d7f4311
fwbuild <fwbuild@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2007-08-27 15:12:40 +0000

    Tagged build
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/v2.0@793 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ v2.0.0.127/FlexWiki.Web/admin/WikiAdminConstants.js
@@ -0,0 +1,6 @@
+/* Constants from ShowCache.aspx.cs.
+   If you update these you MUST update the corresponding
+   constants in ShowCache.aspx.cs */
+var KEYDATAPREFIX = "keyData";
+var KEYPREFIX = "keyRow";
+/* End of ShowCache constants */
