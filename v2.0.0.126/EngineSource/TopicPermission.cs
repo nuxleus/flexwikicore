de74b45d3368a5fbf8b3665e35699ab2b04d2dc1
fwbuild <fwbuild@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2007-08-27 15:08:53 +0000

    Tagged build
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/v2.0@792 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ v2.0.0.126/EngineSource/TopicPermission.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlexWiki
+{
+    public enum TopicPermission
+    {
+        // ManageNamespace = 0,
+        Edit = 1, 
+        Read = 2,
+    }
+}
