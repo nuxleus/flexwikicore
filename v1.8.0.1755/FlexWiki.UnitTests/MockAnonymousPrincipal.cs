802f2331dbb8f8fec52e46fb100fc38b455feb64
fwbuild <fwbuild@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2007-08-29 05:17:41 +0000

    Tagged build
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/v1.8@812 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ v1.8.0.1755/FlexWiki.UnitTests/MockAnonymousPrincipal.cs
@@ -0,0 +1,32 @@
+#region License Statement
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// The use and distribution terms for this software are covered by the 
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.TXT at the root of this distribution.
+// By using this software in any fashion, you are agreeing to be bound by 
+// the terms of this license.
+//
+// You must not remove this notice, or any other, from this software.
+#endregion
+
+using System;
+using System.Security.Principal; 
+
+namespace FlexWiki.UnitTests
+{
+	public class MockAnonymousPrincipal : IPrincipal
+	{
+    private MockAnonymousIdentity _identity = new MockAnonymousIdentity(); 
+
+    public IIdentity Identity
+    {
+      get { return _identity; }
+    }
+
+    public bool IsInRole(string role)
+    {
+      return false;
+    }
+  }
+}
