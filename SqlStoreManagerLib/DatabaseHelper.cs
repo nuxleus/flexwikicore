784544fade3f6adf9c80d4cc8ecc1ba0af1f3921
jwdavidson <jwdavidson@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2008-02-14 17:00:07 +0000

    Bug 1893199 - Unable to build SqlStoreManagerLib. Added SqlStoreManagerLib, SqlStoreManagerConsole and SqlStoeManagerGUI projects to the FlexWikiCore.sln and set project dependencies
    Removed a linefeed from DatabaseHelper.cs in sqlStoreManagerLib to force build.
    
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/trunk@936 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

--- SqlStoreManagerLib/DatabaseHelper.cs
+++ SqlStoreManagerLib/DatabaseHelper.cs
@@ -67,7 +67,6 @@ namespace FlexWiki.SqlStoreManagerLib
     {
       string connectionString = string.Format("server={0};Integrated Security=true", instance);
       SetUpDatabaseInternal(connectionString, instance, database, dataFileDirectory, user);
-
     }
 
     /// <summary>
