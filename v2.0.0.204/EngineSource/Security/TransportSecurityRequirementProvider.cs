500308ae0170defa6b34381f5a01230662188dda
fwbuild <fwbuild@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2008-02-26 16:00:23 +0000

    Tagged build
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/v2.0@946 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ v2.0.0.204/EngineSource/Security/TransportSecurityRequirementProvider.cs
@@ -0,0 +1,243 @@
+#region License Statement
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// The use and distribution terms for this software are covered by the 
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.TXT at the root of this distribution.
+// By using this software in any fashion, you are agreeing to be bound by 
+// the terms of this license.
+//
+// You must not remove this notice, or any other, from this software.
+#endregion
+
+using System;
+using System.IO;
+
+using FlexWiki.Collections;
+
+namespace FlexWiki.Security
+{
+    public class TransportSecurityRequirementProvider : IContentProvider
+    {
+        private const string c_recursionContextKey = "TransportSecurityRequirementProviderRecursionContextKey";
+
+        private NamespaceManager _namespaceManager;
+        private IContentProvider _next;
+
+        public TransportSecurityRequirementProvider(IContentProvider next)
+        {
+            _next = next;
+        }
+
+        public bool Exists
+        {
+            get
+            {
+                using (CreateRecursionContext())
+                {
+                    return _next.Exists;
+                }
+            }
+        }
+        public bool IsReadOnly
+        {
+            get
+            {
+                using (CreateRecursionContext())
+                {
+                    return _next.IsReadOnly;
+                }
+            }
+        }
+        public IContentProvider Next
+        {
+            get
+            {
+                return _next;
+            }
+            set
+            {
+                _next = value;
+            }
+        }
+        public static string RequirementPropertyName
+        {
+            get { return "RequireTransportSecurityFor"; }
+        }
+
+        private bool IsRecursing
+        {
+            get { return RecursionContextHelper.IsRecursing(c_recursionContextKey); }
+        }
+        private IWikiApplication WikiApplication
+        {
+            get { return _namespaceManager.Federation.Application; }
+        }
+
+        public TopicChangeCollection AllChangesForTopicSince(UnqualifiedTopicName topic, DateTime stamp)
+        {
+            using (CreateRecursionContext())
+            {
+                return _next.AllChangesForTopicSince(topic, stamp);
+            }
+        }
+        public QualifiedTopicNameCollection AllTopics()
+        {
+            using (CreateRecursionContext())
+            {
+                return _next.AllTopics();
+            }
+        }
+        public void DeleteAllTopicsAndHistory()
+        {
+            using (CreateRecursionContext())
+            {
+                _next.DeleteAllTopicsAndHistory();
+            }
+        }
+        public void DeleteTopic(UnqualifiedTopicName topic)
+        {
+            using (CreateRecursionContext())
+            {
+                _next.DeleteTopic(topic);
+            }
+        }
+        public ParsedTopic GetParsedTopic(UnqualifiedTopicRevision topicRevision)
+        {
+            using (CreateRecursionContext())
+            {
+                return _next.GetParsedTopic(topicRevision);
+            }
+        }
+        public bool HasNamespacePermission(NamespacePermission permission)
+        {
+            using (CreateRecursionContext())
+            {
+                return _next.HasNamespacePermission(permission); 
+            }
+        }
+        public bool HasPermission(UnqualifiedTopicName topic, TopicPermission permission)
+        {
+            using (CreateRecursionContext())
+            {
+                return _next.HasPermission(topic, permission);
+            }
+        }
+        public void Initialize(NamespaceManager namespaceManager)
+        {
+            _namespaceManager = namespaceManager;
+            _next.Initialize(namespaceManager);
+        }
+        public void LockTopic(UnqualifiedTopicName topic)
+        {
+            using (CreateRecursionContext())
+            {
+                _next.LockTopic(topic);
+            }
+        }
+        public TextReader TextReaderForTopic(UnqualifiedTopicRevision topicRevision)
+        {
+            AssertTransportSecurityRequirement(TransportSecurityContext.Content);
+            using (CreateRecursionContext())
+            {
+                return _next.TextReaderForTopic(topicRevision);
+            }
+        }
+        public bool TopicExists(UnqualifiedTopicName name)
+        {
+            using (CreateRecursionContext())
+            {
+                return _next.TopicExists(name);
+            }
+        }
+        public bool TopicIsReadOnly(UnqualifiedTopicName name)
+        {
+            using (CreateRecursionContext())
+            {
+                return _next.TopicIsReadOnly(name);
+            }
+        }
+        public void UnlockTopic(UnqualifiedTopicName topic)
+        {
+            using (CreateRecursionContext())
+            {
+                _next.UnlockTopic(topic);
+            }
+        }
+        public void WriteTopic(UnqualifiedTopicRevision topicRevision, string content)
+        {
+            using (CreateRecursionContext())
+            {
+                _next.WriteTopic(topicRevision, content);
+            }
+        }
+
+        private void AssertTransportSecurityRequirement(TransportSecurityContext context)
+        {
+            if (IsRecursing)
+            {
+                return;
+            }
+
+            if (IsTransportSecurityRequired(context))
+            {
+                if (!WikiApplication.IsTransportSecure)
+                {
+                    throw new TransportSecurityRequirementException();
+                }
+            }
+        }
+        private IDisposable CreateRecursionContext()
+        {
+            return new RecursionContextHelper(c_recursionContextKey);
+        }
+        private bool IsTransportSecurityRequired(TransportSecurityContext context)
+        {
+            TransportSecurityRequiredFor requiredOn = WikiApplication.FederationConfiguration.RequireTransportSecurityFor;
+
+            using (CreateRecursionContext())
+            {
+                TopicProperty property = _namespaceManager.GetTopicProperty(
+                    _namespaceManager.DefinitionTopicName.LocalName,
+                    RequirementPropertyName);
+
+                if (property != null)
+                {
+                    if (property.HasValue)
+                    {
+                        TransportSecurityRequiredFor value;
+                        if (TryParseValue(property.LastValue, out value))
+                        {
+                            requiredOn = value;
+                        }
+                    }
+                }
+            }
+
+            if (requiredOn == TransportSecurityRequiredFor.Content)
+            {
+                return true; 
+            }
+
+            return false; 
+        }
+
+        private bool TryParseValue(string input, out TransportSecurityRequiredFor value)
+        {
+            value = TransportSecurityRequiredFor.None;
+            
+            try
+            {
+                value = (TransportSecurityRequiredFor) Enum.Parse(typeof(TransportSecurityRequiredFor), input, true);
+                return true; 
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+
+        }
+
+
+    }
+}
---
+++ v2.0.0.204/FlexWiki.Web/admin/Providers.aspx.cs
@@ -0,0 +1,172 @@
+#region License Statement
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//
+// The use and distribution terms for this software are covered by the 
+// Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file CPL.TXT at the root of this distribution.
+// By using this software in any fashion, you are agreeing to be bound by 
+// the terms of this license.
+//
+// You must not remove this notice, or any other, from this software.
+#endregion
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using FlexWiki;
+using System.Configuration;
+using FlexWiki.Web;
+
+
+namespace FlexWiki.Web.Admin
+{
+    /// <summary>
+    /// Summary description for Admin.
+    /// </summary>
+    public class Providers : AdminPage
+    {
+        
+		// Answer an array of arrays.  Each inner array has a collection of NamespaceProviderDefinitions with the same type
+        private List<List<NamespaceProviderDefinition>> ProvidersByType
+        {
+            get
+            {
+                Dictionary<string, List<NamespaceProviderDefinition>> collector =
+                    new Dictionary<string, List<NamespaceProviderDefinition>>();
+                FederationConfiguration fc = Federation.Application.FederationConfiguration;
+                if (fc != null)
+                {
+                    foreach (NamespaceProviderDefinition each in fc.NamespaceMappings)
+                    {
+                        string key = each.Type;
+                        List<NamespaceProviderDefinition> list;
+                        if (collector.ContainsKey(key))
+                        {
+                            list = collector[key];
+                        }
+                        else
+                        {
+                            list = new List<NamespaceProviderDefinition>();
+                            collector[key] = list;
+                        }
+                        list.Add(each);
+                    }
+                }
+                List<List<NamespaceProviderDefinition>> answer = new List<List<NamespaceProviderDefinition>>();
+                List<string> keys = new List<string>();
+                keys.AddRange(collector.Keys);
+                keys.Sort();
+                foreach (string k in keys)
+                {
+                    answer.Add(collector[k]);
+                }
+
+                return answer;
+            }
+        }
+		
+
+		protected override void DefaultPageLoad()
+        {
+            MinimalPageLoad();
+        }
+		
+		protected override void ShowMain()
+        {
+
+            foreach (List<NamespaceProviderDefinition> each in ProvidersByType)
+            {
+                // Lay down the header
+                NamespaceProviderDefinition first = each[0];
+                string s = first.Type;
+                if (first.AssemblyName != null)
+                {
+                    s += " (in " + first.AssemblyName + ")";
+                }
+                UIResponse.WriteHeading(UIResponse.Escape(s), 1);
+
+                UITable table = new UITable();
+                table.AddColumn(new UIColumn()); // edit link
+                List<string> columns = new List<string>();
+                foreach (NamespaceProviderParameter parm in first.Parameters)
+                {
+                    string column = parm.Name;
+                    columns.Add(column);
+                    table.AddColumn(new UIColumn(column));
+                }
+
+                UIResponse.WriteStartTable(table);
+                foreach (NamespaceProviderDefinition inner in each)
+                {
+                    UIResponse.WriteStartRow();
+                    UIResponse.WriteCell(UIResponse.CommandLink("EditProvider.aspx?Provider=" + inner.Id, Command.Edit, "edit this provider's information"));
+                    // Parameters might be listed out of order, so we have to look through
+                    // them all to get the right one. 
+                    foreach (string column in columns)
+                    {
+                        NamespaceProviderParameter parm = GetParameter(inner.Parameters, column);
+                        UIResponse.WriteCell(UIResponse.Escape(parm.Value));
+                    }
+                    UIResponse.WriteEndRow();
+                }
+                UIResponse.WriteEndTable();
+
+            }
+        }
+		
+		protected override void ShowMenu()
+        {
+            UIResponse.WriteStartMenu("Providers");
+            UIResponse.WriteMenuItem("EditProvider.aspx", "Add provider", "Add a new provider instance");
+            UIResponse.WriteEndMenu();
+            UIResponse.WritePara("&nbsp;");
+
+            base.ShowMenu();
+        }
+		private NamespaceProviderParameter GetParameter(ArrayList arrayList, string name)
+        {
+            foreach (NamespaceProviderParameter parameter in arrayList)
+            {
+                if (parameter.Name.Equals(name))
+                {
+                    return parameter;
+                }
+            }
+
+            return null;
+        }
+		
+		private void Page_Load(object sender, System.EventArgs e)
+        {
+        }
+
+
+        #region Web Form Designer generated code
+        override protected void OnInit(EventArgs e)
+        {
+            //
+            // CODEGEN: This call is required by the ASP.NET Web Form Designer.
+            //
+            InitializeComponent();
+            base.OnInit(e);
+        }
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Load += new System.EventHandler(this.Page_Load);
+
+        }
+        #endregion
+    }
+}
