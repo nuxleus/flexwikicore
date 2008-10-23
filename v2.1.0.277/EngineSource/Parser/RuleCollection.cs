1eda0b7a38b3e9b6ebed8567cc1556bcd31efb14
fwbuild <fwbuild@662ad2f3-a61f-0410-a8b9-97bb17fbd72e>
2008-10-23 20:29:47 +0000

    Tagged build
    git-svn-id: https://flexwiki.svn.sourceforge.net/svnroot/flexwiki/flexwikicore/tags/v2.1@1090 662ad2f3-a61f-0410-a8b9-97bb17fbd72e

---
+++ v2.1.0.277/EngineSource/Parser/RuleCollection.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Text;
+
+using FlexWiki;
+
+namespace FlexWiki.Formatting
+{
+    public class RuleCollection : Collection<ParserRule>
+    {
+    }
+}
